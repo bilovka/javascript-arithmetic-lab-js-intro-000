b22e76337385259549a8c7fd2e826542515f099b
Oleksandr <bilovka10@gmail.com>
2020-08-07 17:03:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(){
+  
+}
+function substract(){
+  
+}
+function add(a,b){
+  return a+b
+}
+function subtract(a,b){
+  return a-b
+}
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n){
+  return n+1
+}
+function decrement(n){
+  return n-1
+}
+function makeInt(n){
+  parseInt(n, 10)
+  parseInt('0x2328',10)
+  parseInt('zzz',10)
+}
